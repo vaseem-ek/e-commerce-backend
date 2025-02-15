23622138c41700738b860c3cd2bd67d64fc7f6c2
vaseem <manuknl2@gmail.com>
2025-02-15 16:41:18 +0530

    'stripe'

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,86 +1,136 @@
-const orders=require('../models/orderModel')
+const orders = require('../models/orderModel')
 const users = require('../models/userModel')
+const Stripe = require('stripe')
 
-exports.placeOrder=async(req,res)=>{
+// const currency = "usd"
+// const deliveryCharges = 10
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+exports.placeOrder = async (req, res) => {
     try {
-        const userId=req.payload
-        const {items,amount,address}=req.body
+        const userId = req.payload
+        const { items, amount, address } = req.body
 
-        const orderData={
+        const orderData = {
             userId,
             items,
             amount,
             address,
-            paymentMethod:"COD",
-            payment:false,
-            date:Date.now()
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
         }
 
-        const newOrder=new orders(orderData)
+        const newOrder = new orders(orderData)
         await newOrder.save()
 
-        await users.findByIdAndUpdate(userId,{cartData:{}})
+        await users.findByIdAndUpdate(userId, { cartData: {} })
 
-        return res.status(200).json({success:true,message:"Order placed"})
-        
+        return res.status(200).json({ success: true, message: "Order placed" })
+
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ success: false, message: "Internal server error" })  
+        return res.status(500).json({ success: false, message: "Internal server error" })
     }
 }
 
 
-exports.placeOrderStripe=async(req,res)=>{
+exports.placeOrderStripe = async (req, res) => {
     try {
-        
+        // const userId = req.payload
+        // const { items, amount, address } = req.body
+        // const { origin } = req.headers
+
+        // const orderData = {
+        //     userId,
+        //     items,
+        //     amount,
+        //     address,
+        //     paymentMethod: "Stripe",
+        //     payment: false,
+        //     date: Date.now()
+        // }
+        // const newOrder = new orders(orderData)
+        // await newOrder.save()
+
+        // const line_items = items.map((item) => ({
+        //     price_data: {
+        //         currency: currency,
+        //         product_data: {
+        //             name: item.name
+        //         },
+        //         unit_amount: item.price * 100
+        //     },
+        //     quantity: item.quantity
+        // }))
+        // line_items.push({
+        //     price_data: {
+        //         currency: currency,
+        //         product_data: {
+        //             name: "Delivery Charges"
+        //         },
+        //         unit_amount: deliveryCharges * 100
+        //     },
+        //     quantity: 1
+        // })
+
+        // const session = await stripe.checkout.sessions.create({
+
+        // })
+
+
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ success: false, message: "Internal server error" })  
+        return res.status(500).json({ success: false, message: "Internal server error" })
     }
 }
 
 
-exports.placeOrderRazorepay=async(req,res)=>{
+exports.placeOrderRazorepay = async (req, res) => {
     try {
-        
+
+
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ success: false, message: "Internal server error" })  
+        return res.status(500).json({ success: false, message: "Internal server error" })
     }
 }
 
 
-exports.allOrders=async(req,res)=>{
+exports.allOrders = async (req, res) => {
     try {
-        const allOrder=await orders.find()
-        return res.status(200).json({success:true,allOrder})
+        const allOrder = await orders.find()
+        return res.status(200).json({ success: true, allOrder })
 
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ success: false, message: "Internal server error" })  
+        return res.status(500).json({ success: false, message: "Internal server error" })
     }
 }
 
 
-exports.orderUser=async(req,res)=>{
+exports.orderUser = async (req, res) => {
     try {
-        const userId=req.payload
-        const allOrders=await orders.find({userId})
-        return res.status(200).json({success:true,allOrders})
+        const userId = req.payload
+        const allOrders = await orders.find({ userId })
+        return res.status(200).json({ success: true, allOrders })
 
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ success: false, message: "Internal server error" })  
+        return res.status(500).json({ success: false, message: "Internal server error" })
     }
 }
 
 
-exports.updateStatus=async(req,res)=>{
+exports.updateStatus = async (req, res) => {
     try {
-        
+        const { orderId, status } = req.body
+        await orders.findByIdAndUpdate(orderId, { status })
+        return res.status(200).json({ success: true, message: "Status Updated" })
+
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ success: false, message: "Internal server error" })  
+        return res.status(500).json({ success: false, message: "Internal server error" })
     }
 }
 
